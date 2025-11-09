c584530d81812f546eb51731129278a3844584af
antomondaca <nelamondaca@gmail.com>
2025-11-09 20:27:11 -0300

    a

--- demo/composeApp/src/jvmMain/kotlin/com/example/demo/persistencia/StorageDriverFactory.kt
+++ demo/composeApp/src/jvmMain/kotlin/com/example/demo/persistencia/StorageDriverFactory.kt
@@ -1,10 +1,9 @@
 package com.example.demo.persistencia
 
-/**
- * Implementación "real" (actual) de la fábrica para la plataforma JVM.
- * Cuando el código común llame a createStorageDriver(), esta función se ejecutará.
- * Como estamos en jvmMain, SÍ podemos ver y crear una instancia de FileStorageDriver.
- */
+
+//Implementación "real" (actual) de la fábrica para la plataforma JVM.
+//Cuando el código común llame a createStorageDriver(), esta función se ejecutará.
+//Como estamos en jvmMain, SÍ podemos ver y crear una instancia de FileStorageDriver.
 actual fun createStorageDriver(): IStorageDriver {
     return FileStorageDriver()
 }
--- demo/composeApp/src/jvmMain/kotlin/com/example/demo/servicios/PdfServiceFactory.kt
+++ demo/composeApp/src/jvmMain/kotlin/com/example/demo/servicios/PdfServiceFactory.kt
@@ -1,10 +1,9 @@
 package com.example.demo.servicios
 
-/**
- * Implementación "real" (actual) de la fábrica para la plataforma Desktop.
- * Cuando el código de commonMain llame a createPdfService(), y se esté ejecutando
- * en la JVM, esta función será la que se invoque.
- */
+
+//Implementación "real" (actual) de la fábrica para la plataforma Desktop.
+//Cuando el código de commonMain llame a createPdfService(), y se esté ejecutando
+//en la JVM, esta función será la que se invoque.
 actual fun createPdfService(): PdfService {
     return DesktopPdfService()
 }
