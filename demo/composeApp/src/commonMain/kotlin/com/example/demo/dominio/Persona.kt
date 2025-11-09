9971cc96ecff897610c3a25bdd93dc9316763a06
HiddenOnLies <150493423+HiddenOnLies@users.noreply.github.com>
2025-11-09 23:43:23 +0000

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit a726737621653abceb7a87c0cbf409b40fbc5b07, reversing
    changes made to 5ce6002a05ab7269fc4032c60ec5db17f376a949.

--- demo/composeApp/src/commonMain/kotlin/com/example/demo/dominio/Persona.kt
+++ demo/composeApp/src/commonMain/kotlin/com/example/demo/dominio/Persona.kt
@@ -1,8 +1,8 @@
 package com.example.demo.dominio
 
-/**
- * Clase abstracta que representa a una persona en el sistema.
- */
+
+ //Clase abstracta que representa a una persona en el sistema.
+
 abstract class Persona : EntidadBase() {
     abstract val rut: String
     abstract val nombre: String
